cc0916d13ffc294561e714057db7e1ebcc0f7f85
mishael-cypher <somtookonkwo96@gmail.com>
2023-02-07 18:01:58 +0100

    finished major features

--- App.tsx
+++ App.tsx
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Switch, SafeAreaView } from "react-native";
+import { ThemeContext } from "./context/ThemeContext";
+import { myColors } from "./styles/Colors";
+import MyKeyboard from "./components/MyKeyboard";
 
 export default function App() {
+  const [theme, setTheme] = useState("light");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeContext.Provider value={theme}>
+      <SafeAreaView
+        style={
+          theme === "light"
+            ? styles.container
+            : [styles.container, { backgroundColor: "#000" }]
+        }
+      >
+        {/* <StatusBar style="auto" /> */}
+        <Switch
+          value={theme === "light"}
+          onChange={() => setTheme(theme === "light" ? "dark" : "light")}
+        />
+        <MyKeyboard />
+      </SafeAreaView>
+    </ThemeContext.Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: myColors.light,
+    alignItems: "center",
+    justifyContent: "flex-start",
   },
 });
---
+++ components/Button.tsx
@@ -0,0 +1,46 @@
+import { useContext } from "react";
+import { TouchableOpacity, Text } from "react-native";
+import { ThemeContext } from "../context/ThemeContext";
+import { Styles } from "../styles/GlobalStyles";
+
+interface ButtonProps {
+  onPress: () => void;
+  title: string;
+  isBlue?: boolean;
+  isGrey?: boolean;
+}
+
+export default function Button({
+  title,
+  onPress,
+  isBlue,
+  isGrey,
+}: ButtonProps) {
+  const theme = useContext(ThemeContext);
+  return (
+    <TouchableOpacity
+      style={
+        isBlue
+          ? Styles.btnBlue
+          : isGrey
+          ? Styles.btnGray
+          : theme === "light"
+          ? Styles.btnLight
+          : Styles.btnDark
+      }
+      onPress={onPress}
+    >
+      <Text
+        style={
+          isBlue || isGrey
+            ? Styles.smallTextLight
+            : theme === "dark"
+            ? Styles.smallTextLight
+            : Styles.smallTextDark
+        }
+      >
+        {title}
+      </Text>
+    </TouchableOpacity>
+  );
+}
